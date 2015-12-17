acd1c0ba87be3ca207b176b16c206d4953116d90
Tony Young <ty_training@hotmail.co.uk>
2015-12-17 22:14:54 +0000

    User story 5, multiple links funtionality and Rakefile added

--- app.rb
+++ app.rb
@@ -23,8 +23,9 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.create(url: params[:url], title: params[:title])
-    tag = Tag.create(tag: params[:tag])
-    link.tags << tag
+    params[:tag].split.each do |tag|
+    link.tags << Tag.create(tag: tag)
+   end
     link.save
     redirect '/links'
   end
--- spec/features/filter_links_spec.rb
+++ spec/features/filter_links_spec.rb
@@ -1,5 +1,7 @@
 feature 'filtering of tags' do
-
+  # As a time-pressed user
+  # So that I can quickly find links on a particular topic
+  # I would like to filter links by tag
   scenario 'allows user to filter tags' do
     visit('/links/new')
     fill_in('title', with: 'West Ham')
@@ -9,5 +11,19 @@ feature 'filtering of tags' do
     click_button('Create New')
     visit('/links/bubbles')
     expect(page).to have_content('bubbles')
+  end
+
+  # As a time-pressed user
+  # So that I can organise my links into different categories for ease of search
+  # I would like to add tags to the links in my bookmark manager
+  scenario 'allows user to add multiple tags to a link' do
+    visit('/links/new')
+    fill_in('title', with: 'Jose')
+    fill_in('url', with: 'http://www.bbc.co.uk/sport/0/football/34670192')
+    fill_in('tag', with: 'football sacked')
+
+    click_button('Create New')
+    link = Link.first
+    expect(link.tags.map(&:tag)).to include('football', 'sacked')
   end
 end
